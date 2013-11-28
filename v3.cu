a8ed631a9a111fce8fc559644b83fdc27b714abb
Alexandr Tsidaev <a.tsidaev@gmail.com>
2013-11-29 01:08:23 +0600

    Extended info

--- v3.cu
+++ v3.cu
@@ -38,22 +38,22 @@ float Vz3(float x, float y, float x1, float x2, float y1, float y2, float z1, fl
 }
 
 __global__
-void Calculate(float xLL, float yLL, float xStep, float yStep, float* top, float* bottom, float* result)
+void Calculate(int yLine, float xLL, float yLL, float xStep, float yStep, float* top, float* bottom, float* result)
 {
-	float x = xLL + xStep * blockIdx.x;
-	float y = yLL + yStep * blockIdx.y;
+	float x = xLL + xStep * threadIdx.x;
+	float y = yLL + yStep * yLine;
 	
-	float x1 = xLL + xStep * threadIdx.x;
+	float x1 = xLL + xStep * blockIdx.x;
 	float x2 = x1 + xStep;
 	
-	float y1 = yLL + yStep * threadIdx.y;
+	float y1 = yLL + yStep * blockIdx.y;
 	float y2 = y1 + yStep;
 	
-	int pos_grid = threadIdx.x + threadIdx.y * SIDE;
-	float t = top[pos_grid];
-	float b = bottom[pos_grid];
+	int pos_grid = blockIdx.x + blockIdx.y * SIDE;
+	float t = 1; //top[pos_grid];
+	float b = 0; //bottom[pos_grid];
 	
-	int pos_result = blockIdx.x + blockIdx.y * SIDE;
+	int pos_result = threadIdx.x + yLine * SIDE;
 	
 	float r = Vz3(x, y, x1, x2, y1, y2, t, b, 0);
 	
@@ -63,6 +63,13 @@ void Calculate(float xLL, float yLL, float xStep, float yStep, float* top, float
 
 int main()
 {
+	cudaDeviceProp props;
+	cudaGetDeviceProperties(&props, 0);
+	printf("%s API version %d.%d\n", props.name, props.major, props.minor);
+        
+	printf("Max block dimensions: [%d, %d, %d]\n", props.maxThreadsDim[0], props.maxThreadsDim[1], props.maxThreadsDim[2]);
+	printf("Threads per block: %d\n", props.maxThreadsPerBlock);
+	
 	float *result = (float*)malloc(SIDE * SIDE * dsize);
 	memset(result, 0, SIDE * SIDE * dsize);
 	
@@ -75,16 +82,19 @@ int main()
 	cudaMemcpy(resultd, result, SIDE * SIDE * dsize, cudaMemcpyHostToDevice);
 	cudaMemcpy(catd, cat, SIDE * SIDE * dsize, cudaMemcpyHostToDevice);
 	
-	dim3 blocks(1, 1);
-	dim3 threads(1, 1);
+	dim3 blocks(256, 256);
+	dim3 threads(256);
 	
 	//Vz3<<<blocks,threads>>>(0, 0, -100000000, 100000000, -100000000, 100000000, 1, 0, 0, result);
 	//V<<<1,1>>>(result);
-	Calculate<<<blocks,threads>>>(10000, 11000, 4, 4, zerod, catd, resultd);
+	Calculate<<<blocks,threads>>>(128, 10000, 11000, 4, 4, zerod, catd, resultd);
+	
+	cudaError_t error = cudaGetLastError();
+	printf("Error: %s\n", cudaGetErrorString(error));
 	
 	cudaMemcpy(result, resultd, SIDE * SIDE * dsize, cudaMemcpyDeviceToHost);
 	cudaFree( resultd );
-	printf("%f\n", result[0]);
+	printf("%f\n", result[128 * 256 + 128]);
 	
 	return 0;
 }
