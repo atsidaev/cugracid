c4d75e607f4a82b065d451729289bb80f1f9dd18
Alexander Tsidaev <atsidaev@gmail.com>
2013-12-11 19:25:46 +0600

    m_to_km program

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ NVCFLAGS=-arch=sm_21
 
 LDFLAGS=-L$(CUDA_INSTALL_PATH)/lib64 -lcudart
 
-all:	v3
+all:	v3 m_to_km
 
 clean:
 	find -name '*.o' -delete
@@ -15,3 +15,5 @@ v3:	v3.o cuda/info.o cuda/Vz.o grid/Grid.o
 
 %.o:	%.cu
 	$(NVCC) -c $(NVCFLAGS) $^ -o $@
+
+m_to_km:	grid/Grid.o
---
+++ m_to_km.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+#include "grid/Grid.h"
+
+int main(int argc, char** argv)
+{
+	if (argc != 3)
+	{
+		cout << "Usage: m_to_km <m.grd> <km.grd>" << endl;
+		return 1;
+	}
+	
+	Grid g(argv[1]);
+	
+	g.xLL /= 1000;
+	g.yLL /= 1000;
+	g.xSize /= 1000;
+	g.ySize /= 1000;
+	
+	g.Write(argv[2]);
+}
