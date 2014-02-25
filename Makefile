c9159cb046e8a897dccb53940f9875ad220b2047
Alexander Tsidaev <atsidaev@gmail.com>
2014-02-26 00:40:00 +0600

    Local corrections program

--- Makefile
+++ Makefile
@@ -5,13 +5,15 @@ NVCFLAGS=-arch=sm_21
 
 LDFLAGS=-L$(CUDA_INSTALL_PATH)/lib64 -lcudart
 
-all:	v3 m_to_km
+all:	v3 lc  m_to_km
 
 clean:
 	find -name '*.o' -delete
 	rm v3
 
 v3:	v3.o direct.o cuda/info.o cuda/Vz.o grid/Grid.o
+
+lc:	lc.o direct.o cuda/info.o cuda/Vz.o grid/Grid.o
 
 %.o:	%.cu
 	$(NVCC) -c $(NVCFLAGS) $^ -o $@
---
+++ lc.cpp
@@ -0,0 +1,75 @@
+#include <cstring>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <mpi.h>
+
+#include "global.h"
+#include "cuda/Vz.h"
+#include "cuda/info.h"
+
+#include "grid/Grid.h"
+
+#include "direct.h"
+
+int main(int argc, char** argv)
+{
+	int mpi_rank, mpi_size;
+
+	MPI_Init (&argc, &argv);      /* starts MPI */
+	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);        /* get current process id */
+	MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);        /* get number of processes */
+
+	cudaPrintInfo();
+	if (argc < 6)
+	{
+		printf("Usage: lc <field.grd> <boundary.grd> <density> <alpha> <iterations> [output.grd]\n");
+		return 1;
+	}
+	char* fieldFilename = argv[1];
+	char* boundaryFilename = argv[2];
+	double dsigma = atof(argv[3]);
+	double alpha = atof(argv[4]);
+	double iterations = atoi(argv[5]);
+	
+	char* outputFilename = NULL;
+	if (argc > 6)
+		outputFilename = argv[6];
+
+	Grid observedField(fieldFilename);
+	Grid modelBoundary(boundaryFilename);
+
+	Grid boundary(boundaryFilename);
+	
+	printf("Field grid read\n");
+	
+	for (int i = 0; i < iterations; i++)
+	{
+		printf("Iteration %d\n", i);
+		FLOAT* result = CalculateDirectProblem(boundary, dsigma, mpi_rank, mpi_size);
+
+		if (mpi_rank == MPI_MASTER)
+		{
+			for (int j = 0; j < boundary.nCol * boundary.nRow; j++)
+				boundary.data[j] /= (1 + alpha * (observedField.data[j] - result[j]));
+		
+			delete result;
+		}
+	
+	}
+	
+	if (mpi_rank == MPI_MASTER)
+	{
+		boundary.zMin = boundary.get_Min();
+		boundary.zMax = boundary.get_Max();
+		boundary.Write(outputFilename);
+	}
+	
+	MPI_Finalize();
+	
+	
+	return 0;
+}
+
